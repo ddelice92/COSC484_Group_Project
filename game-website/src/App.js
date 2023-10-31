6128524205e5c616ee9c1c3b3814410a6f2535ba
marksalamander <143564826+marksalamander@users.noreply.github.com>
2023-10-31 19:23:04 -0400

    Add files via upload

--- game-website/src/App.js
+++ game-website/src/App.js
@@ -1,21 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Main from './pages/homepage.js';
+import Homepage from './pages/homepage.js';
 import User from './pages/user';
 import Login from './pages/login';
 import TicTacToe from './pages/TicTacToe';
 import Checkers from './pages/checkers';
 import ConnectFour from './pages/connect-four';
+import Signup from './pages/signup';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route index element={<Main />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/user" element={<User />} />
+          <Route index element={<Homepage />} />
+          <Route path="/homepage" element={<Homepage />} />
+          <Route exact path="/user" element={<User />} />
           <Route path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
           <Route path="/TicTacToe" element={<TicTacToe />} />
           <Route path="/checkers" element={<Checkers />} />
           <Route path="/connect-four" element={<ConnectFour />} />
