56da81137099e5eca61fc35f10db993d2fd2038f
marksalamander <143564826+marksalamander@users.noreply.github.com>
2023-11-21 17:14:30 -0500

    Add files via upload

--- game-website/src/pages/checkers.js
+++ game-website/src/pages/checkers.js
@@ -1,10 +1,17 @@
 import React from "react"
 import Header from "../components/header"
+import Checkerboard from "../components/checkerboard.js"
+import s from "../CSS/checkers.module.css"
 
 export default function Checkers() {
     return (
         <div>
             <Header />
+            <div id={s.container}>
+                <div id={s.canvasContainer}>
+                    <Checkerboard />
+                </div>
+            </div>
         </div>
     )
 }
