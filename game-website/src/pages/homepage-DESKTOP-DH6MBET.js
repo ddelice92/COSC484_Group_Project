66c16d6c586270c45f4c2fe71b84b2e896380508
doughnuttouch <67760292+doughnuttouch@users.noreply.github.com>
2023-12-01 01:58:50 -0500

    basic checkers backend

---
+++ game-website/src/pages/homepage-DESKTOP-DH6MBET.js
@@ -0,0 +1,60 @@
+import React from "react"
+import "../App.css"
+import "../CSS/homepage.css"
+import Header from "../components/header"
+import TicTacToe from "../images/TicTacToe.png"
+import Checkers from "../images/checker-board.png"
+import ConnectFour from "../images/ConnectFour.jpg"
+
+export default function Homepage() {
+    return (
+        <div id="root">
+            <head>
+            </head>
+                <Header />
+            <body>
+                <div id="bodyContainer">
+                    <div id="listContainer">
+                        
+                        <div class="gameContainer">
+                            <div class="game">
+                                <a href='/TicTacToe'>
+                                    <img class="gameImage" src={TicTacToe} alt="Tic-Tac-Toe"></img>
+                                </a>
+                                <div class="gameImage-outline"></div>
+                            </div>
+                            <div class="gameName">
+                                Tic-Tac-Toe
+                            </div>
+                        </div>
+
+                        <div class="gameContainer">
+                            <div class="game">
+                                <a href="/checkers">
+                                    <img class="gameImage" src={Checkers} alt="Checkers"></img>
+                                </a>
+                                <div class="gameImage-outline"></div>
+                            </div>
+                            <div class="gameName">
+                                Checkers
+                            </div>
+                        </div>
+
+                        <div class="gameContainer">
+                            <div class="game">
+                                <a href="/connect-four">
+                                    <img class="gameImage" src={ConnectFour} alt="Connect Four"></img>
+                                </a>
+                                <div class="gameImage-outline"></div>
+                            </div>
+                            <div class="gameName">
+                                Connect Four
+                            </div>
+                        </div>
+        
+                    </div>
+                </div>
+            </body>
+        </div>
+    )
+}
